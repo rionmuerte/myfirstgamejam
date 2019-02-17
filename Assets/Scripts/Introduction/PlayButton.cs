337cd8d9f8d2bb5c52107de1891e736f7a8434ad
jolaff <joel.landgraf@gmail.com>
2019-02-17 07:56:02 -0300

    fixed untracked files

--- Assets/Scripts/Introduction/PlayButton.cs
+++ Assets/Scripts/Introduction/PlayButton.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
     public void LoadIntroduction() => SceneManager.LoadScene("Introduction");
+
 }
---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void BackToMain() => SceneManager.LoadScene("Main");
+
+    public void BonusButton() => SceneManager.LoadScene("Bonus");
+
+    public void QuitGame() => Application.Quit();
+    
+}
